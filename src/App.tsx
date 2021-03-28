7a2464f58a0b2e2746219d5ae31c58924e62a26e
Fabio <offabio@outlook.com>
2021-03-28 16:47:52 +0200

    feat: added home functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,128 @@
-import React from 'react'
+import React, { createRef, useEffect, useRef, useState } from 'react'
 import { render } from 'react-dom'
 import { GlobalStyle } from './styles/GlobalStyle'
-
-import Greetings from './components/Greetings'
+import { Button, Grid, makeStyles, Snackbar, TextField, Typography } from '@material-ui/core'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
+import JSONInput from 'react-json-editor-ajrm'
+import locale from 'react-json-editor-ajrm/locale/en'
+import ReactJson from 'react-json-view'
+import MuiAlert from '@material-ui/lab/Alert'
+const WebSocket = require('ws')
 
 const mainElement = document.createElement('div')
 mainElement.setAttribute('id', 'root')
 document.body.appendChild(mainElement)
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: '#5a4f7c'
+    }
+  }
+})
+
+const useStyles = makeStyles({
+  root: {
+    '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+      borderColor: '#5a4f7c'
+    },
+    '& .Mui-focused': {
+      color: '#e7e7e7'
+    }
+  }
+})
+
 const App = () => {
+  const classes = useStyles()
+  const [url, setUrl] = useState<string>('')
+  const [data, setData] = useState<any>({})
+  const [socketData, setSocketData] = useState({})
+
+  const [open, setOpen] = React.useState(false)
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    setOpen(false)
+  }
+
+  const [ws, setWs] = useState<WebSocket>()
+
+  function connectToSocket () {
+    if (url.length > 1) {
+      try {
+        const server = new WebSocket(url)
+
+        setWs(server)
+        server.on('open', () => server.send('Hello from desktop'))
+        server.on('message', (msg) => {
+          try {
+            const parsed = JSON.parse(msg)
+            setSocketData(parsed)
+          } catch (e) {
+            setSocketData({message: msg})
+          }
+        })
+        setOpen(true)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+
+  function sendData () {
+    ws?.send(data)
+  }
+
   return (
-    <>
-      <GlobalStyle />
-      <Greetings />
-    </>
+    <ThemeProvider theme={theme}>
+      <Grid container style={{ margin: 10, width: '90vw', marginLeft: 'auto', marginRight: 'auto' }}>
+        <Grid item xs={10}>
+          <form style={{ marginRight: 10 }}>
+            <TextField id="outlined-basic"
+              label="Enter websocket url"
+              variant="outlined"
+              className={classes.root}
+              fullWidth
+              onChange={(x) => setUrl(x.target.value)}
+              InputLabelProps={{
+                style: { color: theme.palette.primary.main }
+              }}
+            />
+          </form>
+        </Grid>
+        <Grid item xs>
+          <Button variant={'contained'} color={'primary'} onClick={() => connectToSocket()}>Connect</Button>
+        </Grid>
+      </Grid>
+      <Grid container style={{ margin: 10, width: '90vw', marginLeft: 'auto', marginRight: 'auto' }} spacing={2}>
+        <Grid item>
+          <Typography variant="h5" component="h2">
+            Data
+          </Typography>
+          <JSONInput
+            id='a_unique_id'
+            locale={ locale }
+            height='550px'
+            onChange={(item) => setData(item.json)}
+          />
+          <div style={{ marginTop: 10, marginLeft: 'auto' }}>
+            <Button variant={'contained'} color={'primary'} onClick={() => sendData()}>Send data</Button>
+          </div>
+        </Grid>
+        <Grid item>
+          <ReactJson src={socketData} theme={'ocean'}/>
+        </Grid>
+      </Grid>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <MuiAlert elevation={6} variant="filled">Connected to the socket!</MuiAlert>
+      </Snackbar>
+      <GlobalStyle/>
+    </ThemeProvider>
   )
 }
 
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -10,6 +10,36 @@ export const GlobalStyle = createGlobalStyle`
   body {
     font-family: Arial, Helvetica, sans-serif;
     font-size: 16px;
-    color: #E1E1E6;
+    background-color: #121212;
+    color: #e7e7e7;
+  }
+  
+  //input {
+  //  margin: 10px;
+  //  padding: 10px;
+  //  background-color: rgba(0,0,0,0.1);
+  //  border-top: none;
+  //  border-left: none;
+  //  border-right: none;
+  //  border-bottom: 2px solid #5a4f7c;
+  //  color: #e7e7e7;
+  //}
+  
+  button {
+    padding: 10px;
+    border: none;
+    background-color: #5a4f7c;
+    color: white;
+    height: 52px;
+    width: 150px;
+    font-size: 20px;
+    font-weight: bold;
+  }
+  
+  #header {
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 1%;
   }
 `
